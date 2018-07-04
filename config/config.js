74dec02483253138b4bdee50833f5b000c8a8b7f
QuanTM9 <quantm9@fsoft.com.vn>
2018-07-04 11:15:27 +0700

    config heroku

--- config/config.js
+++ config/config.js
@@ -6,11 +6,11 @@ CONFIG.app = process.env.APP || 'development';
 CONFIG.port = process.env.PORT || '3000';
 
 CONFIG.db_dialect = process.env.DB_DIALECT || 'mongo';
-CONFIG.db_host = process.env.DB_HOST || 'yourdoctordb.documents.azure.com';
-CONFIG.db_port = process.env.DB_PORT || '10255';
-CONFIG.db_name = process.env.DB_NAME || 'yourdoctordbfinal';
-CONFIG.db_user = process.env.DB_USER || 'yourdoctordb';
-CONFIG.db_password = process.env.DB_PASSWORD || 'bk0zHAmbw4UIoRkJwqUZOugw3oMqZaI7enwnhFb70v0NSYmIOdt6aXvG2aJGtT4tiquCuoYxgo5ucSdxnc0b1g%3D%3D';
+CONFIG.db_host = process.env.DB_HOST || 'ds125841.mlab.com';
+CONFIG.db_port = process.env.DB_PORT || '25841';
+CONFIG.db_name = process.env.DB_NAME || 'heroku_9s7l353d';
+CONFIG.db_user = process.env.DB_USER || 'yourdoctoruser';
+CONFIG.db_password = process.env.DB_PASSWORD || '123abc';
 
 CONFIG.jwt_encryption = process.env.JWT_ENCRYPTION || '123@123';
 CONFIG.jwt_expiration = process.env.JWT_EXPIRATION || '10000';
