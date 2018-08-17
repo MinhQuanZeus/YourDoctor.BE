d63f540dda9c55048064f6d642258267d89c5c2a
QuanTMSE04347 <quantmse04347@fpt.edu.vn>
2018-08-17 13:18:14 +0700

    add deletion flag for bank and introduce

--- models/Bank.js
+++ models/Bank.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 let BankSchema = mongoose.Schema({
     nameBank: {type: String},
-    nameBankEnglish:{type: String},
-    nameTransaction:{type: String}
+    nameBankEnglish: {type: String},
+    nameTransaction: {type: String},
+    deletionFlag: {type: Boolean, default: false}
 }, {timestamps: true});
 
 let Bank = module.exports = mongoose.model('Bank', BankSchema);
--- models/IntroduceAndRule.js
+++ models/IntroduceAndRule.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 let introAndRuleSchema = mongoose.Schema({
     type: {type: String},
-    content: {type: String}
+    content: {type: String},
+    deletionFlag: {type: Boolean, default: false}
 });
 
 let IntroduceAndRule = module.exports = mongoose.model('IntroduceAndRule', introAndRuleSchema);
